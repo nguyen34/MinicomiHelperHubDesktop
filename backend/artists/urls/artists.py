a8d6a7e8121bdc260d806dad68bbd58427d450d3
nguyen34 <nguyenjohnson65@gmail.com>
2023-10-02 14:24:01 -0700

    Added missing change

--- backend/artists/urls/artists.py
+++ backend/artists/urls/artists.py
@@ -2,5 +2,5 @@ from django.urls import path
 import artists.views as views
 
 urlpatterns = [
-    path("artists/test/", views.test_endpoint),
+    path("artists/test/", views.test_endpoint, name="test_endpoint"),
 ]
