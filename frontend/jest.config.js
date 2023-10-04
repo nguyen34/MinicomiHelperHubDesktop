468388bc9a7f8d8160cf2599a5c0ab401ea4fa81
nguyen34 <nguyenjohnson65@gmail.com>
2023-10-04 15:41:03 -0700

    Example Store Tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     "^.+\\.vue$": "@vue/vue3-jest",
     "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.ts?$': 'ts-jest',
   },
   testEnvironment: "jsdom",
   testEnvironmentOptions: {
--- frontend/src/store/modules/example.ts
+++ frontend/src/store/modules/example.ts
@@ -6,11 +6,18 @@ export default {
     mutations: {
         increment(state: any) {
             state.count++;
+        },
+        decrement(state: any) {
+            state.count--;
         }
     },
     actions: {
         increment(context: any) {
             context.commit('increment');
-        }
+        },
+
+        decrement(context: any) {
+            context.commit('decrement');
+        },
     }
 };
